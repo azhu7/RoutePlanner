86d7f610f7939b87fc5eb350605b0cd95e888e60
Albert Lo <lohoi@umich.edu>
2017-09-22 22:31:56 -0400

    running app

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-server.js
-
 // set up ========================
 var express  = require('express');
 var app      = express();                               // create our app w/ express
@@ -12,16 +10,16 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 
 // mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 // app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-app.get('*',function(req,res)) {
-    res.sendfile('index.html');
-}
+app.get('/',function(req,res) {
+    res.sendFile( __dirname + '/index.html');
+})
 
 
 // listen (start app with node server.js) ======================================
