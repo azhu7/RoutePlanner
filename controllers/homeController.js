aca492d218d298bc1d091cda63bcaf4ff98da153
Jerry Meng <ymeng@umich.edu>
2017-09-23 12:19:05 -0400

    smh ngmap

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,24 +1,32 @@
 app.controller('HomeCtrl', function($scope,$http,$state) {
-    $scope.submitForm = function() {
-        $http.get("/login", $scope.user).then(function(response) {
-            // $state.go('map')
-            console.log("login successful");
-        });
-        console.log("user email: " + $scope.user.email);
-    }
+	$scope.submitForm = function() {
+		$http.get("/login", $scope.user).then(function(response) {
+			// $state.go('map')
+			console.log("login successful");
+		});
+		console.log("user email: " + $scope.user.email);
+	}
 
-    $scope.user = {};
+	$scope.user = {};
 });
 
 // helper to initialize the Google Maps Api through
 // MapCtrl
 
-app.controller('MapCtrl', function($scope,NgMap) {
-    $scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCRJ1VRQTn7e9QJaHecb9ZXGtUg4rtzIpA"
+app.controller('MapCtrl', function($scope, NgMap) {
+	$scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBNbbLZzrEcAlt149RElY0jnHmZ3ADzoYw"
 
-    NgMap.getMap().then(function(map) {
-        console.log('markers' , map.markers);
-    })
+	$scope.types = "['establishment']";
+
+	$scope.placeChanged = function() {
+		$scope.place = this.getPlace();
+		console.log('location', $scope.place.geometry.location);
+		$scope.map.setCenter($scope.place.geometry.location);
+	};
+
+	NgMap.getMap().then(function(map) {
+		console.log('markers' , map.markers);
+	});
 
 });
 
