adb961e1f6031612415a6dc0438ee1376bc73e2a
Albert Lo <lohoi@umich.edu>
2017-09-23 14:48:29 -0400

    yup

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -19,14 +19,28 @@ app.controller('MapCtrl', function($scope, NgMap) {
 
     $scope.placeChanged = function() {
         $scope.vm.place = this.getPlace();
+        console.log($scope.vm.place.geometry.location)
         marker = new google.maps.Marker({
-            position: $scope.vm.place.geometry.location,
-            map: $scope.vm.map,
-            draggable: true,
-            animation: google.maps.Animation.DROP
-        });
-        $scope.markers.push()
-        $scope.vm.map.setCenter($scope.vm.place.geometry.location);
+                position: $scope.vm.place.geometry.location,
+                map: $scope.vm.map,
+                draggable: true,
+                animation: google.maps.Animation.DROP,
+              });
+
+        if ($scope.markers.length) {
+            var request = {
+                origin: $scope.markers[$scope.markers.length - 1],
+                destination: marker.position,
+                travelMode: 'DRIVING'
+            }
+            directionsDisplay = new google.maps.DirectionsRenderer().route(request,function(result,status){
+                if(status == 'OK') {
+                    $scope.vm.map.setDirections(result)
+                }
+             });
+        }
+        $scope.markers.push(marker)
+        $scope.vm.map.setZoom(10)
     }
     NgMap.getMap().then(function(map) {
         $scope.vm.map = map;
