800afc639dc3f3259866b9ed280fd5899c49fc90
Jerry Meng <ymeng@umich.edu>
2017-09-23 03:16:21 -0400

    fixed merge conflict

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 var app = angular.module("webApp", ['ui.router', 'ui.bootstrap']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
-	$stateProvider
-		.state('home', {
-			url: '/',
-			templateUrl: 'views/home.html',
-			controller: 'HomeCtrl'
-		}).state('about', {
-			url: '/about',
-			templateUrl: 'views/about.html'
-		}).state('contact',{
-			url: '/contact',
-			templateUrl: 'views/contact.html'
-		}).state('resume', {
-			url: '/resume',
-			templateUrl: 'views/resume.html'
-		});
-	$urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state('home', {
+        url: '/',
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+    }).state('map', {
+        url: '/map',
+        templateUrl: 'views/map.html',
+	controller: 'MapCtrl'
+    });
+
+    $urlRouterProvider.otherwise('/');
 });
 
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -10,3 +10,42 @@ app.controller('HomeCtrl', function($scope,$http,$state) {
     $scope.user = {};
 });
 
+// helper to initialize the Google Maps Api through
+// MapCtrl
+
+app.controller('MapCtrl', function($scope) {
+	$scope.markers = [];
+
+	$scope.initMap = function() {
+		var myLatlng = {lat: -25.363, lng: 131.044};
+
+
+		$scope.map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 4,
+			center: myLatlng
+		});
+
+		google.maps.event.addListener($scope.map, 'click', function(event) {
+			$scope.placeMarker(event.latLng);
+		});
+
+		$scope.addMarker = function (marker) {
+			$scope.markers.push("karat");
+		}
+
+		$scope.placeMarker = function (location) {
+			var marker = new google.maps.Marker({
+				position: location, 
+				map: $scope.map
+			});
+
+			$scope.addMarker(marker);
+		}
+
+	}
+
+	$scope.initMap();
+
+
+
+});
