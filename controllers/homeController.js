eac398440da08bddb95651ee3d4e51c58565043d
Albert Lo <lohoi@umich.edu>
2017-09-23 12:48:38 -0400

    map works

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,32 +1,29 @@
 app.controller('HomeCtrl', function($scope,$http,$state) {
-	$scope.submitForm = function() {
-		$http.get("/login", $scope.user).then(function(response) {
-			// $state.go('map')
-			console.log("login successful");
-		});
-		console.log("user email: " + $scope.user.email);
-	}
+    $scope.submitForm = function() {
+        $http.get("/login", $scope.user).then(function(response) {
+            // $state.go('map')
+            console.log("login successful");
+        });
+        console.log("user email: " + $scope.user.email);
+    }
 
-	$scope.user = {};
+    $scope.user = {};
 });
 
 // helper to initialize the Google Maps Api through
 // MapCtrl
 
 app.controller('MapCtrl', function($scope, NgMap) {
-	$scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBNbbLZzrEcAlt149RElY0jnHmZ3ADzoYw"
-
-	$scope.types = "['establishment']";
-
-	$scope.placeChanged = function() {
-		$scope.place = this.getPlace();
-		console.log('location', $scope.place.geometry.location);
-		$scope.map.setCenter($scope.place.geometry.location);
-	};
-
-	NgMap.getMap().then(function(map) {
-		console.log('markers' , map.markers);
-	});
-
+    var vm = this;
+    vm.types = "['establishment']";
+    vm.placeChanged = function() {
+        console.log("Sup!!!")
+        console.log(this.getPlace())
+        vm.place = this.getPlace();
+        console.log('location', vm.place.geometry.location);
+        vm.map.setCenter(vm.place.geometry.location);
+    }
+    NgMap.getMap().then(function(map) {
+        vm.map = map;
+    });
 });
-
